b11cab9db131b7599d89a6dab73a0e5b92a2d6a2
Fahim <fahimashraph65@gmail.com>
2026-04-23 15:49:30 +0300

    fix signup fields

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -11,7 +11,7 @@ const [grade, setGrade] = useState("");
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
 
